52cde5adc5d32181875315e29e46c0bda4ffc534
beginnerpyth <hskx7779@gmail.com>
2025-12-29 20:15:40 +0900

    Add files via upload
    just made a simple game

---
+++ game.py
@@ -0,0 +1,15 @@
+secret = "giraffe"
+guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = False
+while guess != secret and not(out_of_guesses):
+    if guess_count < guess_limit :
+        guess = input("enter a namer")
+        guess_count += 1
+    else:
+        out_of_guesses = True
+if out_of_guesses:
+        print("out of gueeses you lost")
+else:
+        print("you win")
